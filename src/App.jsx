662969bdd5515983dc7e581b8ae52a24fe995976
merndevreza <merndevreza@gmail.com>
2024-03-03 16:33:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <> 
+      <h1 className="bg-green-200 py-3 text-center font-medium text-3xl">Uncommon React Hooks</h1>
+      
+    </>
+  )
+}
+
+export default App
